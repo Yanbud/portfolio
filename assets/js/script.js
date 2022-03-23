d3b20ea502e6267a38a888be16adc7521222f8e5
Yan Zhang <yannbuda@gmail.com>
2022-03-23 19:16:34 -0400

    Updated my work

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ const mod1 = document.querySelector('#modal1')
 const close1 = document.querySelector('#close1')
 const mod2 = document.querySelector('#modal2')
 const close2 = document.querySelector('#close2')
+const mod3 = document.querySelector('#modal3')
+const close3 = document.querySelector('#close3')
 const display1 = () => {
     mod1.setAttribute('style', 'display:block');
 }
@@ -15,4 +17,11 @@ const display2 = () => {
 
 const closeMod2 = () => {
     mod2.setAttribute('style', 'display:none');
+}
+const display3 = () => {
+    mod3.setAttribute('style', 'display:block');
+}
+
+const closeMod3 = () => {
+    mod3.setAttribute('style', 'display:none');
 }
