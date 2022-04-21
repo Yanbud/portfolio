ffa67b85631dae8849cbb6bcda62d16288f2274c
Yan Zhang <yannbuda@gmail.com>
2022-04-21 10:01:03 -0400

    changed the images to responsive in models

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ const mod2 = document.querySelector('#modal2')
 const close2 = document.querySelector('#close2')
 const mod3 = document.querySelector('#modal3')
 const close3 = document.querySelector('#close3')
+const mod4 = document.querySelector('#modal4')
+const close4 = document.querySelector('#close4')
+const mod5 = document.querySelector('#modal5')
+const close5 = document.querySelector('#close5')
 const display1 = () => {
     mod1.setAttribute('style', 'display:block');
 }
@@ -24,4 +28,18 @@ const display3 = () => {
 
 const closeMod3 = () => {
     mod3.setAttribute('style', 'display:none');
+}
+const display4 = () => {
+    mod4.setAttribute('style', 'display:block');
+}
+
+const closeMod4 = () => {
+    mod4.setAttribute('style', 'display:none');
+}
+const display5 = () => {
+    mod5.setAttribute('style', 'display:block');
+}
+
+const closeMod5 = () => {
+    mod5.setAttribute('style', 'display:none');
 }
