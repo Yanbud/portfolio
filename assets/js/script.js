4b88ef8fdd014f0a0406f0d48de4ba16b4ef8b14
Yan Zhang <yannbuda@gmail.com>
2022-04-26 09:05:29 -0400

    Updated logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
-const mod1 = document.querySelector('#modal1')
-const close1 = document.querySelector('#close1')
-const mod2 = document.querySelector('#modal2')
-const close2 = document.querySelector('#close2')
-const mod3 = document.querySelector('#modal3')
-const close3 = document.querySelector('#close3')
-const mod4 = document.querySelector('#modal4')
-const close4 = document.querySelector('#close4')
-const mod5 = document.querySelector('#modal5')
-const close5 = document.querySelector('#close5')
+const mod1 = document.querySelector('#modal1');
+const close1 = document.querySelector('#close1');
+const mod2 = document.querySelector('#modal2');
+const close2 = document.querySelector('#close2');
+const mod3 = document.querySelector('#modal3');
+const close3 = document.querySelector('#close3');
+const mod4 = document.querySelector('#modal4');
+const close4 = document.querySelector('#close4');
+const mod5 = document.querySelector('#modal5');
+const close5 = document.querySelector('#close5');
+const year = document.querySelector('#year');
+const full = new Date().getFullYear();
+year.textContent = full;
 const display1 = () => {
     mod1.setAttribute('style', 'display:block');
 }
